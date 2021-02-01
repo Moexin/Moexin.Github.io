0afe34e82b4bbb48f22d99470e2a2c76b30824b2
Moexin <moexin@qq.com>
2021-02-01 15:05:37 +0800

    Update

--- conf.py
+++ conf.py
@@ -3,41 +3,41 @@
 """
 
 # For Maverick
-site_prefix = "/Blog-With-GitHub-Boilerplate/"
+site_prefix = "/"
 source_dir = "../src/"
 build_dir = "../dist/"
-index_page_size = 10
-archives_page_size = 20
+index_page_size = 5
+archives_page_size = 15
 template = {
     "name": "Galileo",
     "type": "local",
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "Moexin/Moexin.Github.io@gh-pages"
 }
 
 # 站点设置
-site_name = "我的个人博客"
-site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+site_name = "Moexin's Blog"
+site_logo = "${static_prefix}Moexin.jpg"
+site_build_date = "2016-01-22T16:51+08:00"
+author = "Moexin"
+email = "i@dqb.pw"
+author_homepage = "https://dqb.pw"
+description = "心若浮沉💗浅笑安然"
+key_words = ['Moexin', 'Blog', "Moexin's Blog", '萌新']
 language = 'zh-CN'
 external_links = [
     {
-        "name": "Maverick",
-        "url": "https://github.com/AlanDecode/Maverick",
-        "brief": "🏄‍ Go My Own Way."
+        "name": "TCat云服务",
+        "url": "https://tcat.pw",
+        "brief": "可能是最走心的云服务商"
     },
     {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
+        "name": "Moexin's NetDisk",
+        "url": "https://pan.dqb.pw",
+        "brief": "基于OneDrive的个人网盘"
     }
 ]
 nav = [
@@ -60,19 +60,9 @@ nav = [
 
 social_links = [
     {
-        "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
-        "icon": "gi gi-twitter"
-    },
-    {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/Moexin",
         "icon": "gi gi-github"
-    },
-    {
-        "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
-        "icon": "gi gi-weibo"
     }
 ]
 
