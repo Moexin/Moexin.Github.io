ebe3df4a411797aeefb91bb99e12698240932c96
Moexin <moexin@qq.com>
2021-02-01 15:33:27 +0800

    Update

--- conf.py
+++ conf.py
@@ -78,6 +78,26 @@ valine = {
 head_addon = r'''
 <meta http-equiv="x-dns-prefetch-control" content="on">
 <link rel="dns-prefetch" href="//cdn.jsdelivr.net" />
+<link rel="shortcut icon" href="${site_prefix}Favicon/favicon.ico">
+<link rel="icon" sizes="16x16 32x32 64x64" href="${site_prefix}Favicon/favicon.ico">
+<link rel="icon" type="image/png" sizes="196x196" href="${site_prefix}Favicon/favicon-192.png">
+<link rel="icon" type="image/png" sizes="160x160" href="${site_prefix}Favicon/favicon-160.png">
+<link rel="icon" type="image/png" sizes="96x96" href="${site_prefix}Favicon/favicon-96.png">
+<link rel="icon" type="image/png" sizes="64x64" href="${site_prefix}Favicon/favicon-64.png">
+<link rel="icon" type="image/png" sizes="32x32" href="${site_prefix}Favicon/favicon-32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="${site_prefix}Favicon/favicon-16.png">
+<link rel="apple-touch-icon" href="${site_prefix}Favicon/favicon-57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="${site_prefix}Favicon/favicon-114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="${site_prefix}Favicon/favicon-72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="${site_prefix}Favicon/favicon-144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="${site_prefix}Favicon/favicon-60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="${site_prefix}Favicon/favicon-120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="${site_prefix}Favicon/favicon-76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="${site_prefix}Favicon/favicon-152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="${site_prefix}Favicon/favicon-180.png">
+<meta name="msapplication-TileColor" content="#FFFFFF">
+<meta name="msapplication-TileImage" content="${site_prefix}Favicon/favicon-144.png">
+<meta name="msapplication-config" content="${site_prefix}Favicon/browserconfig.xml">
 '''
 
 footer_addon = ''
